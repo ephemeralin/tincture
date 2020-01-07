42bea9342efec8e1b04e39be4df015605939fec0
ephemeralin <ephemeralin@gmail.com>
2020-01-07 18:26:10 +0300

    cors proxy fix

--- src/App.js
+++ src/App.js
@@ -26,8 +26,8 @@ class App extends Component {
 
     render() {
         const navbarTabId = this.state.navbarTabId;
-        console.log("ENV " + process.env.CORS_PROXY_URL);
-        console.log("PROP " + corsProxyUrl);
+        // console.log("ENV " + process.env.CORS_PROXY_URL);
+        // console.log("PROP " + corsProxyUrl);
         const corsProxyUrlParam = process.env.CORS_PROXY_URL ? process.env.CORS_PROXY_URL : corsProxyUrl;
         let panel =
             <PanelTech corsProxyUrl={corsProxyUrlParam}></PanelTech>;
